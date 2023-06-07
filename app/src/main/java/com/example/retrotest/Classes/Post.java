46873d219c54c733a7c9c334c03f343d265a8042
Tomer <tomer.kamar@gmail.com>
2023-06-07 11:51:38 +0300

    create post class

---
+++ app/src/main/java/com/example/retrotest/Classes/Post.java
@@ -0,0 +1,30 @@
+package com.example.retrotest.Classes;
+
+public class Post {
+
+    private String title;
+    private String body;
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Post setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Post setBody(String body) {
+        this.body = body;
+        return this;
+    }
+}
