e0b4a1e73a2d139143277d316d6c0f36bc3f2829
Tomer <tomer.kamar@gmail.com>
2023-06-07 12:21:45 +0300

    Create MainActivity logic

--- app/src/main/java/com/example/retrotest/MainActivity.java
+++ app/src/main/java/com/example/retrotest/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.retrotest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.retrotest.Classes.Post;
+import com.example.retrotest.Classes.PostAdapter;
+import com.example.retrotest.Services.RetrofitClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+    LinearLayoutManager layoutManager;
+    PostAdapter adapter;
+    List<Post> postList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        initObj();
+        fetchPosts();
+
+    }
+
+    private void fetchPosts() {
+        progressBar.setVisibility(View.VISIBLE);
+        RetrofitClient.getRetrofitClient().getPosts().enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    postList.addAll(response.body());
+                    adapter.notifyDataSetChanged();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Error" + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initObj() {
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new PostAdapter(postList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void findViews() {
+        recyclerView = findViewById(R.id.recycleView);
+        progressBar = findViewById(R.id.progressBar);
     }
 }
