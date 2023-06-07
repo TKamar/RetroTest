2c9bf17c369b476a3a5a759c7eca57f7928750ce
Tomer <tomer.kamar@gmail.com>
2023-06-07 12:21:57 +0300

    Add GET method to api interface

--- app/src/main/java/com/example/retrotest/Interfaces/ApiInterface.java
+++ app/src/main/java/com/example/retrotest/Interfaces/ApiInterface.java
@@ -1,5 +1,15 @@
 package com.example.retrotest.Interfaces;
 
+import com.example.retrotest.Classes.Post;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiInterface {
+
+    @GET("/posts")
+    Call<List<Post>> getPosts();
 
 }
